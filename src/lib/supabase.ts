8a10cc1d539489273686b74cf0e2e9af67013912
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 07:55:45 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string;
-
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Faltou VITE_SUPABASE_URL ou VITE_SUPABASE_ANON_KEY no .env");
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 
