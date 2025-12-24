a3c4967750f07addbd21acbb66af3d47a96aa5f7
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 13:34:58 -0300

    Update Risks.tsx

--- src/pages/Risks.tsx
+++ src/pages/Risks.tsx
@@ -1,17 +1,10 @@
-import Sidebar from "../components/Sidebar";
-
 export default function Risks() {
   return (
-    <div className="di-layout">
-      <Sidebar />
-      <main className="di-main">
-        <div className="container">
-          <div className="card" style={{ display: "grid", gap: 10 }}>
-            <h1>Riscos (PGR / NR-01)</h1>
-            <p>Página base OK ✅</p>
-          </div>
-        </div>
-      </main>
+    <div className="container">
+      <div className="card" style={{ display: "grid", gap: 10 }}>
+        <h1>Riscos (PGR / NR-01)</h1>
+        <p>Página base OK ✅</p>
+      </div>
     </div>
   );
 }
