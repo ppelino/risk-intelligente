67665b4e710914898aabefa60324fb8ad7463fa9
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 07:49:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Companies from "./pages/Companies";
+import Sectors from "./pages/Sectors";
+import Risks from "./pages/Risks";
+import Ergonomics from "./pages/Ergonomics";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/companies" element={<Companies />} />
+      <Route path="/sectors" element={<Sectors />} />
+      <Route path="/risks" element={<Risks />} />
+      <Route path="/ergonomics" element={<Ergonomics />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
+
 
 
 
