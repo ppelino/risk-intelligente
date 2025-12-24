66229d67edf2c2330fe7ca27c87ae47efaa33f5f
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 13:26:57 -0300

    Update Sectors.tsx

--- src/pages/Sectors.tsx
+++ src/pages/Sectors.tsx
@@ -1,12 +1,10 @@
 export default function Sectors() {
   return (
-    <main className="di-main">
-      <div className="container">
-        <div className="card" style={{ display: "grid", gap: 10 }}>
-          <h1>Setores / Funções</h1>
-          <p>Página base OK ✅</p>
-        </div>
+    <div className="container">
+      <div className="card" style={{ display: "grid", gap: 10 }}>
+        <h1>Setores / Funções</h1>
+        <p>Página base OK ✅</p>
       </div>
-    </main>
+    </div>
   );
 }
