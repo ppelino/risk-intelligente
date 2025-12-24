8096e49b19ae2a519d939ed7fb2518c503293c5d
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 13:25:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Companies from "./pages/Companies";
 import Sectors from "./pages/Sectors";
+import Risks from "./pages/Risks";
+import Ergonomics from "./pages/Ergonomics";
 import AppShell from "./components/AppShell";
 import RequireAuth from "./lib/RequireAuth";
 
@@ -11,17 +13,22 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
 
-      <Route element={<RequireAuth />}>
-        <Route element={<AppShell />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/companies" element={<Companies />} />
-          <Route path="/sectors" element={<Sectors />} />
-        </Route>
+      <Route
+        element={
+          <RequireAuth>
+            <AppShell />
+          </RequireAuth>
+        }
+      >
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/sectors" element={<Sectors />} />
+        <Route path="/risks" element={<Risks />} />
+        <Route path="/ergonomics" element={<Ergonomics />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
-
