d8ea2c1ef6c31cc85a674d1244cee37712eed710
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 07:51:12 -0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,13 +2,24 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Login</h1>
-      <p>Se você está vendo isso, o Router está OK.</p>
+    <div className="di-layout">
+      <main className="di-main">
+        <div className="container">
+          <div className="card" style={{ display: "grid", gap: 10 }}>
+            <h1>Login</h1>
+            <p>Se você está vendo isso, o Router está OK.</p>
 
-      <Link to="/dashboard">Ir para Dashboard</Link>
+            {/* Por enquanto é só navegação (depois entra Supabase aqui) */}
+            <Link to="/dashboard" style={{ fontWeight: 700 }}>
+              Ir para Dashboard
+            </Link>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
+
+
 
 
