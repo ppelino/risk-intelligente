3d2759b50f8e61fc848158782061f4991814d48e
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 08:12:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,34 @@ import Sectors from "./pages/Sectors";
 import Risks from "./pages/Risks";
 import Ergonomics from "./pages/Ergonomics";
 
+import RequireAuth from "./lib/RequireAuth";
+import AppShell from "./components/AppShell";
+
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
 
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/companies" element={<Companies />} />
-      <Route path="/sectors" element={<Sectors />} />
-      <Route path="/risks" element={<Risks />} />
-      <Route path="/ergonomics" element={<Ergonomics />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <AppShell />
+          </RequireAuth>
+        }
+      >
+        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="companies" element={<Companies />} />
+        <Route path="sectors" element={<Sectors />} />
+        <Route path="risks" element={<Risks />} />
+        <Route path="ergonomics" element={<Ergonomics />} />
+      </Route>
 
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
-
-
 
 
 
