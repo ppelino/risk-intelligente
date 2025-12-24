ffdef0d19b637d126746800aad93ce938fffab04
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-24 10:48:29 -0300

    Update Sectors.tsx

--- src/pages/Sectors.tsx
+++ src/pages/Sectors.tsx
@@ -1,17 +1,12 @@
-import Sidebar from "../components/Sidebar";
-
 export default function Sectors() {
   return (
-    <div className="di-layout">
-      <Sidebar />
-      <main className="di-main">
-        <div className="container">
-          <div className="card" style={{ display: "grid", gap: 10 }}>
-            <h1>Setores / Funções</h1>
-            <p>Página base OK ✅</p>
-          </div>
+    <main className="di-main">
+      <div className="container">
+        <div className="card" style={{ display: "grid", gap: 10 }}>
+          <h1>Setores / Funções</h1>
+          <p>Página base OK ✅</p>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
