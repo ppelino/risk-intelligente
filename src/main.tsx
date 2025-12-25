e87debe9ee9cc27eb267755debedf2254a9fadc2
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-12-25 10:20:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -11,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
 
